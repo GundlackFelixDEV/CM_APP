56643f393f48837fce8b7616b11c5365a7c10c2d
Tirunellai Rajamani, Srividya (HC SI DC IN SV AS) <srividya.tirunellai@siemens-healthineers.com>
2019-10-20 09:52:39 +0200

    Add connection to firebase database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MSTracerComponent} from './mstracer/app.component.mstracer';
+import { MSTracerComponent } from './mstracer/app.component.mstracer';
+import { LifestyleComponent } from './lifestylemgmt/lifestyle.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MSTracerComponent
-  ],
+  declarations: [AppComponent, MSTracerComponent, LifestyleComponent],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'fightclub-20'),
+    AngularFireDatabaseModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule
@@ -21,5 +24,4 @@ import {MSTracerComponent} from './mstracer/app.component.mstracer';
   providers: [],
   bootstrap: [AppComponent]
 })
-
-export class AppModule { }
+export class AppModule {}
